eeace67c9490ce71518d5fff157418a3a0285ed5
Carlos Diaz <92885752+Soyelchamps@users.noreply.github.com>
2022-12-13 11:09:11 -0600

    BackEnd Complete

--- src/graphql/resolvers/Mutation.js
+++ src/graphql/resolvers/Mutation.js
@@ -1,19 +1,15 @@
 import movies_catalog from "../../models/movies_catalog";
 import Note from "../../models/Note";
 const Mutation = {
-  async createNote(_, { input }) {
-    // {title: test, description: destest ...... }
-    return await Note.create(input);
-  },
-  async updateNote(_, { input, id }) {
-    return await Note.findByIdAndUpdate(id, input, { new: true });
-  },
-  async removeNote(_, { id }) {
-    await Note.findByIdAndDelete(id);
-    return await Note.find();
-  },
   async createMovie(_, { input }) {
     return await movies_catalog.create(input);
+  },
+  async updateMovie(_, { input, id }) {
+    return await movies_catalog.findByIdAndUpdate(id, input, { new: true });
+  },
+  async removeMovie(_, { id }) {
+    await movies_catalog.findByIdAndDelete(id);
+    return await movies_catalog.find();
   },
 };
 export default Mutation;
--- src/graphql/resolvers/Query.js
+++ src/graphql/resolvers/Query.js
@@ -6,20 +6,15 @@ const Query = {
   ping() {
     return "pong";
   },
-  async getNotes() {
-    const note = await Note.find();
-    return note;
-  },
+
   async getMovies() {
     const Movie = await movies_catalog.find();
     return Movie;
   },
-  // async getNoteById(_, { id }) {
-  //   return await Note.findById(id);
-  // },
-  async getMovieById(_, { _id }) {
-    return await movies_catalog.findById(_id);
+  async getMovieById(_, { id }) {
+    return await movies_catalog.findById(id);
   },
+
   async login(_, { email, passowrd }) {
     const verifyUser = await User.find({ email: email, passowrd: passowrd });
     console.log(verifyUser);
