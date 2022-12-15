398dcb2cf0232441e043af91549ef231a0bac8cb
Carlos Diaz <92885752+Soyelchamps@users.noreply.github.com>
2022-12-14 22:47:06 -0600

    BackEnd Finale

--- src/graphql/resolvers/Mutation.js
+++ src/graphql/resolvers/Mutation.js
@@ -1,5 +1,5 @@
 import movies_catalog from "../../models/movies_catalog";
-import Note from "../../models/Note";
+
 const Mutation = {
   async createMovie(_, { input }) {
     return await movies_catalog.create(input);
@@ -10,6 +10,18 @@ const Mutation = {
   async removeMovie(_, { id }) {
     await movies_catalog.findByIdAndDelete(id);
     return await movies_catalog.find();
+  },
+  async updateLikesMovieById(_, { id }) {
+    const Movie = await movies_catalog.findById(id);
+    const likes = Movie.likes;
+    Movie.likes = likes + 1;
+
+    const res = await movies_catalog.findByIdAndUpdate(
+      id,
+      { likes: Movie.likes },
+      { new: true }
+    );
+    return res;
   },
 };
 export default Mutation;
